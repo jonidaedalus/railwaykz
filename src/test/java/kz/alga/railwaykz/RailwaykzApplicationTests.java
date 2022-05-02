dad69515efde976238945f78434a623de8f4a4e7
jonidaedalus <zhanibek.kalkayev@nu.edu.kz>
2022-05-02 21:25:54 +0600

    Initial commit

---
+++ src/test/java/kz/alga/railwaykz/RailwaykzApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.alga.railwaykz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwaykzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
